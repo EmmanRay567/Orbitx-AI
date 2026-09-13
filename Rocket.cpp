7436f1edcbf302afaf9c551511d3e6d0704d5646
Emmanuel Edward Ray <emmanray567@gmail.com>
2026-09-12 23:59:55 -0400

    Create Rocket.cpp

---
+++ Rocket.cpp
@@ -0,0 +1,10 @@
+#include "Rocket.h"
+Rocket Falcon9("Falcon 9", RocketType::Chemical, RocketStage::FirstStage, 549054, 385000, 385000, 7607000, 8000);
+Rocket Starship("Starship", RocketType::Chemical, RocketStage::SecondStage, 1200000, 1200000, 1200000, 15000000, 10000);
+Rocket SLS("SLS", RocketType::Chemical, RocketStage::ThirdStage, 2600000, 2600000, 2600000, 8000000, 11000);
+Rocket NewGlenn("New Glenn", RocketType::Chemical, RocketStage::FourthStage, 4500000, 4500000, 4500000, 10000000, 12000);
+Rocket Electron("Electron", RocketType::Electric, RocketStage::FirstStage, 12500, 12500, 12500, 162000, 7000);
+Rocket Ariane5("Ariane 5", RocketType::Chemical, RocketStage::SecondStage, 777000, 777000, 777000, 13300000, 9000);
+Rocket LongMarch5("Long March 5", RocketType::Chemical, RocketStage::ThirdStage, 869000, 869000, 869000, 12000000, 9500);
+Rocket H3("H3", RocketType::Chemical, RocketStage::FourthStage, 1000000, 1000000, 1000000, 15000000, 10500);
+Rocket Vega("Vega", RocketType::Chemical, RocketStage::FirstStage, 13700, 13700, 13700, 1400000, 8000);
