7e636786a9ca8299a9e0ce1a861e2ce8197af2b9
Emmanuel Edward Ray <emmanray567@gmail.com>
2026-09-12 23:59:30 -0400

    Create Planet.cpp

---
+++ Planet.cpp
@@ -0,0 +1,10 @@
+#include "Planet.h"	
+Planet Mercury(Planets::Mercury, "Mercury", 3.3011e23, 2.4397e6, 5.791e10, 7.60052e6, 5.431e6, 440, 4.25e3);
+Planet Venus(Planets::Venus, "Venus", 4.8675e24, 6.0518e6, 1.082e11, 1.94142e7, -2.243e6, 737, 10.36e3);
+Planet Earth(Planets::Earth, "Earth", 5.97237e24, 6.371e6, 1.496e11, 3.15576e7, 8.64e4, 288, 11.19e3);
+Planet Mars(Planets::Mars, "Mars", 6.4171e23, 3.3895e6, 2.279e11, 5.93552e7, 8.64e4, 210, 5.03e3);
+Planet Jupiter(Planets::Jupiter, "Jupiter", 1.8982e27, 6.9911e7, 7.785e11, 3.7432e8, 3.57e4, 165, 59.5e3);
+Planet Saturn(Planets::Saturn, "Saturn", 5.6834e26, 5.8232e7, 1.433e12, 9.292e8, 3.57e4, 134, 35.5e3);
+Planet Uranus(Planets::Uranus, "Uranus", 8.6810e25, 2.5362e7, 2.872e12, 2.651e9, -6.48e4, 76, 21.3e3);
+Planet Neptune(Planets::Neptune, "Neptune", 1.02413e26, 2.4622e7, 4.495e12, 5.204e9, 5.97e4, 72, 23.5e3);
+
